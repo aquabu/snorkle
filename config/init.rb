4ef3c7b9c63329684197f0a3d33262d7f682d867
Noah Thorp <noahthorp@rix-10.gateway.2wire.net>
2009-10-31 19:05:32 -0700

    Added bloopsaphone theme song example.

--- config/init.rb
+++ config/init.rb
@@ -1,5 +1,10 @@
 require 'rubygems'
 require 'pp'
+
+# bloopsaphone
+require 'bloops'
+
+
 PROJECT_ROOT = File.expand_path(File.dirname(__FILE__)) + "/../"
 LIB_ROOT = PROJECT_ROOT + "/lib"
 # require all the files in the lib directory
