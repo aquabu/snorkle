49b5d7200a47cd9c363199cc36b5390e5f4fa8da
Noah Thorp <noahthorp@rix.local>
2009-11-20 15:07:55 -0800

    euclidean bloopsaphone reformat print lilypond string add Gamelan Scheduler to scruby example

--- examples/lilypond/simple_score.rb
+++ examples/lilypond/simple_score.rb
@@ -2,4 +2,5 @@ require File.expand_path(File.dirname(__FILE__)) + '/../../config/init.rb'
 
 @lilypond = Lilypond.new(:note_string => "c d e f g", :header => "")
 @lilypond.render
+pp @lilypond.lilypond_doc
 system "open #{@lilypond.output_file}"
--- examples/scruby/bell_tone.rb
+++ examples/scruby/bell_tone.rb
@@ -1,9 +1,15 @@
+# scruby example from Mario Ortega
+# plus some scheduling using the Gamelan::Scheduler
+# Note - I needed to launch the supercollider app
+# and start the localhost server
+# to get this to work
+
 require File.expand_path(File.dirname(__FILE__)) + '/../../config/init.rb'
 require 'scruby'
+require 'gamelan'
 
 s = Server.new
 s.boot
-
 SynthDef.new :fm do |freq, amp, dur|
   mod_env = EnvGen.kr Env.new( d(600, 200, 100), d(0.7,0.3) ), 1, :timeScale => dur
   mod     = SinOsc.ar freq * 1.4, :mul => mod_env
@@ -13,4 +19,15 @@ SynthDef.new :fm do |freq, amp, dur|
   Out.ar  0, [sig, sig]
 end.send
 
-Synth.new :fm, :freq => 220, :amp => 0.4, :dur => 1
+sched = Gamelan::Scheduler.new(:tempo => 60)
+song = [220, 440, 660, 500, 660]
+
+song.each_with_index do |freq, i|
+  sched.at(i) { Synth.new :fm, :freq => freq, :amp => 0.4, :dur => 2 }
+end
+
+sched.at(song.length + 1) { sched.stop }
+
+sched.run
+
+sched.join
